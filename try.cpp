49af14745a72ec975a8063fe81f07ba9acbd3520
Adarsh <puranikadarshk@gmail.com>
2023-02-17 22:49:26 +0530

    Create try.cpp

---
+++ try.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully changed and commit\n";
+  cout << "PES2UG20CS487";
+  return 0;
+}
